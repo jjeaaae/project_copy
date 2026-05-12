c6d3586632df9e89487e830063bafc7608c6d7cc
enhjargal <enhjargal90703039@gmail.com>
2026-05-13 07:25:43 +0800

    Added API routes and frontend fetch

--- app/api/recipes/[id]/route.js
+++ app/api/recipes/[id]/route.js
@@ -1,4 +1,4 @@
-import { getRecipeById } from "@../../../../data";
+import { getRecipeById } from "@../../../data";
 
 export async function GET(request, { params }) {
   const recipe = getRecipeById(params.id);
